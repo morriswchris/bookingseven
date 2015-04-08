ea81a733874bbb37955c311c7b29dd67741e8f50
Chris Morris <cmorris@financeit.io>
2015-04-07 23:59:30 -0400

    added in when booking opens if not open yet

--- bookerseven.js
+++ bookerseven.js
@@ -14,11 +14,20 @@
             confirmButton = "<a href='https://houseseven.com/e/"+data.id+"/confirm' class='simple-button' style='float: right;'>Confirm Booking</a> ";
             $event.find(".event-listing-information").append(confirmButton);
           }
-          else if(data.booking.satte === "confirmed" && $event) {
+          else if(data.booking.state === "confirmed" && $event) {
             confirmButton = "<a href='https://houseseven.com/e/"+data.id+"/cancel' class='simple-button button-cancel' style='float: right;'>Cancel Booking</a> ";
             $event.find(".event-listing-information").append(confirmButton);
           }
           maxGuests = data.booking.tickets - 1 + " / " + maxGuests;
+        }
+        if(data.booking_availability === "booking_not_yet_open"){
+          $.get("/events/" + data.id + "/booking", function(html){
+            var $data = $(html),
+              date = $data.find(".booking-booking_not_yet_open");
+              if(date && date[0]){
+                $event.find(".event-listing-information").append("<span class='event-listing-badge event-listing-booked visible' style='float:left;position: relative !important;margin-left: 80px;'>" + date[0].innerText.trim() + "</span>");
+              }
+          });
         }
         data.message += guestsStr + maxGuests + guestsStrEnd;
       }
